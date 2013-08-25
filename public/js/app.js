df15925b858fd7a70bb790651308befddb51d9ac
Kensuke Nagae <kyanny@gmail.com>
2013-08-26 04:06:56 +0900

    fetch update
    not work Feed ID update :(

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,7 @@
 $(function() {
-  function Feed(id, subject, url) {
+  function Feed(id) {
     var self = this;
     self.id = id;
-    self.name = name;
-    self.url = url;
 
     self.fetch = function() {
       return $.get('/api/feeds/' + self.id);
@@ -12,15 +10,18 @@ $(function() {
   
   function FeedViewModel() {
     var self = this;
-    self.feed = new Feed(1, '', '');
-    self.id = self.feed.id;
+    self.id = 1,
     self.items = ko.observableArray();
 
-    self.feed.fetch().done(function(data) {
-      for (var i = 0; i < data.items.length; i++) {
-        self.items.push(data.items[i]);
-      }
-    });
+    self.fetchFeed = function() {
+      var feed = new Feed(1);
+      feed.fetch().done(function(data) {
+        self.id = feed.id + ":";
+        for (var i = 0; i < data.items.length; i++) {
+          self.items.push(data.items[i]);
+        }
+      });
+    };
   };
 
   ko.applyBindings(new FeedViewModel());
